4b9ed2753968862e84c2147cfde49caf4e671599
Steven Ha <ming@Stevens-MacBook-Pro.local>
2021-03-05 15:50:47 +0000

    user registration and authentication

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,21 +1,23 @@
-import "dotenv/config";
 import { createAuth } from "@keystone-next/auth";
+import { config, createSchema } from "@keystone-next/keystone/schema";
 import {
   withItemData,
   statelessSessions,
 } from "@keystone-next/keystone/session";
-import { User } from "./schemas/User";
-import { Product } from "./schemas/Product";
 import { ProductImage } from "./schemas/ProductImage";
-
-import { config, createSchema } from "@keystone-next/keystone/schema";
+import { Product } from "./schemas/Product";
+import { User } from "./schemas/User";
+import "dotenv/config";
 import { insertSeedData } from "./seed-data";
+import { sendPasswordResetEmail } from "./lib/mail";
+
+function check(name: string) {}
 
 const databaseURL =
   process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
 
 const sessionConfig = {
-  maxAge: 60 * 60 * 24 * 360, // How long should they stay signed import '
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
   secret: process.env.COOKIE_SECRET,
 };
 
@@ -25,12 +27,19 @@ const { withAuth } = createAuth({
   secretField: "password",
   initFirstItem: {
     fields: ["name", "email", "password"],
-    // TODO: add in initial roles here.
+    // TODO: Add in inital roles here
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      // send the email
+      await sendPasswordResetEmail(args.token, args.identity);
+    },
   },
 });
 
 export default withAuth(
   config({
+    // @ts-ignore
     server: {
       cors: {
         origin: [process.env.FRONTEND_URL],
@@ -41,28 +50,26 @@ export default withAuth(
       adapter: "mongoose",
       url: databaseURL,
       async onConnect(keystone) {
-        console.log("I have connected to the database!!");
+        console.log("Connected to the database!");
         if (process.argv.includes("--seed-data")) {
           await insertSeedData(keystone);
         }
       },
     },
     lists: createSchema({
-      // scheme items go in here
+      // Schema items go in here
       User,
       Product,
       ProductImage,
     }),
     ui: {
-      // Show ui only for people who pass this test
-      isAccessAllowed: ({ session }) => {
-        console.log(session);
-        return !!session?.data;
-      },
+      // Show the UI only for poeple who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
     },
-    // TODO: Add session values here
     session: withItemData(statelessSessions(sessionConfig), {
-      // graphql query
+      // GraphQL Query
       User: "id name email",
     }),
   })
--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,13 +1,32 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import SignOut from "./SignOut";
 import NavStyles from "./styles/NavStyles";
+import { useUser } from "./User";
 
 export default function Nav() {
+  const user = useUser();
+
+  // useEffect(() => {
+  //   console.log("Current User", user);
+  // }, [user]);
   return (
     <NavStyles>
       <Link href='/products'>Products</Link>
-      <Link href='/sell'>Sell</Link>
-      <Link href='/orders'>Orders</Link>
-      <Link href='/account'>Account</Link>
+      {user && (
+        <>
+          <Link href='/sell'>Sell</Link>
+          <Link href='/orders'>Orders</Link>
+          <Link href='/account'>Account</Link>
+          <SignOut />
+        </>
+      )}
+
+      {!user && (
+        <>
+          <Link href='/signin'>Sign In</Link>
+        </>
+      )}
     </NavStyles>
   );
 }
